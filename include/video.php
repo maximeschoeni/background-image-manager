84d320c18bf47eb8a40de93e4421765be4a0e0ae
Maxime Schoeni <maxime4@gmail.com>
2019-05-06 09:59:17 +0200

    video playsinline

--- include/video.php
+++ include/video.php
@@ -1,1 +1,1 @@
-<video loop autoplay muted <?php foreach ($attributes as $key => $value) { ?><?php echo $key; ?>="<?php echo $value; ?>" <?php } ?>>
+<video loop autoplay muted playsinline <?php foreach ($attributes as $key => $value) { ?><?php echo $key; ?>="<?php echo $value; ?>" <?php } ?>>
